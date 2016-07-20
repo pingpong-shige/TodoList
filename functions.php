6ca5fe9c9f1f0fe3419eafb28fa285efe2191bed
pingpong-shige <fx591276@gmail.com>
2016-07-21 00:29:17 +0900

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,5 +1,15 @@
 <?php
 
+
+function postgresql_db_connect() {
+
+$dbh = pg_connect("host=localhost port=5432 dbname=todolist user=postgres password=postgresql");
+
+return $dbh;
+
+}
+
+/*
 function db_connect() {
 
     $dsn = 'mysql:dbname=todolist;host=localhost;charset=utf8';
@@ -12,21 +22,6 @@ function db_connect() {
     $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
     return $dbh;
-
 }
+*/
 
-function postgresql_db_connect() {
-
-
-// $conn_string = "host=localhost port=5432 dbname=todolist user=postgres password=postgresql";
-$dbh = pg_connect("host=localhost port=5432 dbname=todolist user=postgres password=postgresql");
-
-//$dbh = "pgsql:dbname=todolist host=localhost user=postgres password=postgresql options='--client_encoding=UTF8'";
-
-
-//$dbconn5 = pg_connect("host=localhost options='--client_encoding=UTF8'");
-// "localhost" のデータベースに接続する際に、エンコーディングを UTF-8 に指定
-
-return $dbh;
-
-}
--- index.php
+++ index.php
@@ -26,27 +26,15 @@ if(isset($_POST['submit'])){
     	$conn = postgresql_db_connect();
 
     	$sql = "INSERT INTO tasks (name, memo,done) VALUES('{$name}','{$memo}',0)";
-//     	$stmt = $conn->prepare($sql);
 		$stmt = pg_query($conn,$sql);
-
-//     	$stmt->bindValue(1, $name, PDO::PARAM_STR);
-//     	$stmt->bindValue(2, $memo, PDO::PARAM_STR);
-//     	$stmt->execute();
 
     	$conn = null;
 
     	unset($name, $memo);
-
     }
 }
 
 if(isset($_POST['method']) && ($_POST['method'] === 'put')){
-
-
-// echo "<pre>";
-// var_dump($_POST);
-// echo "</pre>";
-
 
 	$id = $_POST["id"];
     $id = htmlspecialchars($id, ENT_QUOTES);
@@ -55,16 +43,10 @@ if(isset($_POST['method']) && ($_POST['method'] === 'put')){
     $conn = postgresql_db_connect();
 
     $sql = "UPDATE tasks SET done = 1  WHERE id = {$id}";
-//     $stmt = $conn->prepare($sql);
-
-
-//      $stmt->bindValue(1, $id, PDO::PARAM_INT);
-//     $stmt->execute();
 
     $stmt = pg_query($conn,$sql);
 
     $conn = null;
-
 }
 
 ?>
@@ -101,8 +83,6 @@ if(isset($errors)){
 $conn = postgresql_db_connect();
 
 $sql = 'SELECT id, name, memo, done FROM tasks WHERE done = 0 ORDER BY id DESC';
-//$stmt = $conn->prepare($sql);
-//$stmt->execute();
 
 $stmt = pg_query($conn,$sql);
 if(!$stmt){
@@ -113,16 +93,7 @@ pg_close($conn);
 
 print('<dl>');
 
-
-//while($task = $stmt->fetch(PDO::FETCH_ASSOC)){
 while ($task = pg_fetch_row($stmt)){
-
-
-
-// 	echo "<pre>";
-// 	echo var_dump($task);
-// 	echo "</pre>";
-
 
     print '<dt>';
     print $task[1];
@@ -132,16 +103,15 @@ while ($task = pg_fetch_row($stmt)){
     print $task[2];
     print '</dd>';
 
-print '<dd>';
+	print '<dd>';
     print '
             <form action="index.php" method="post">
             <input type="hidden" name="method" value="put">
             <input type="hidden" name="id" value="' . $task[0] . '">
-            <button type="submit">済んだ</button>
+            <button type="submit">完了</button>
             </form>
           ' ;
     print '</dd>';
-
 }
 
 print('</dl>');
